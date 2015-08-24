fa80a70455e4011d4fdf84f44c33ff256762a663
MAREK <masitko@gmail.com>
2015-08-24 23:35:22 +0100

    added image to recommended

--- code/imdbapi/src/services/Crawler.php
+++ code/imdbapi/src/services/Crawler.php
@@ -6,26 +6,29 @@ use Sunra\PhpSimple\HtmlDomParser;
 
 class Crawler {
 
-    public function getRecommendations($html) {
+    public function getRecommendations($html, $html2 = null) {
 
         $dom = HtmlDomParser::str_get_html($html);
+        $dom2 = HtmlDomParser::str_get_html($html2);
         
-        $elem = $dom->find('.rec_overviews');
+        $elem = $dom->find('.rec_overviews', 0);
+        $elem2 = $dom2->find('#sims', 0);
         
         $result = [];
-        foreach( $elem[0]->children as $child ) {
-            $result[] = $this->parseRecommendation($child);
+        foreach( $elem->children as $child ) {
+            $result[] = $this->parseRecommendation($child, $elem2 );
         }
 
        return json_encode($result);
     }
 
     
-    private function parseRecommendation( $node ) {
+    private function parseRecommendation( $node, $elem2 ) {
         
+        $id = $node->attr["data-tconst"];
         $directors = explode(':',$node->find('.rec-director', 0)->plaintext);
         $rec = [
-            'imdbId' => $node->attr["data-tconst"],
+            'imdbId' => $id,
             'title'  => $node->find('.rec-title > a > b', 0)->plaintext,
             'years'  => $node->find('.rec-title > span.nobr', 0)->plaintext,
             'genres' => $node->find('.rec-cert-genre', 0)->plaintext,
@@ -33,6 +36,7 @@ class Crawler {
             'outline' => $node->find('.rec-outline > p', 0)->plaintext,
             'actors' => trim(ltrim($node->find('.rec-actor', 0)->plaintext, 'Stars: ')),
             'director' => sizeof($directors)>1?trim($directors[1]):"",
+            'image' => $elem2->find('li[data-tconst="'.$id.'"] > img', 0)->attr["src"]
         ];
         
         return $rec;
