84eab626e222d178ff465b7a405734834128147f
MAREK <masitko@gmail.com>
2015-12-11 00:15:59 +0000

    added angular controller, fixed vagrant ssh

--- code/movie-rex/web/app/home/controllers.js
+++ code/movie-rex/web/app/home/controllers.js
@@ -6,5 +6,8 @@ var homeControllers = angular.module( 'homeControllers', [] );
 homeControllers.controller('SearchController', ['$scope',
   function ($scope) {
       
+      $scope.titleChange = function() {
+          console.log(this.movieTitle);          
+      };
       
   }]);
