97e71cd3c696e32d4bd5dc4de6272c94a7a8e610
masitko <masitko@gmail.com>
2015-12-11 16:54:35 +0000

    added resource and some angular template

--- code/movie-rex/web/app/home/controllers.js
+++ code/movie-rex/web/app/home/controllers.js
@@ -1,13 +1,24 @@
 
-var homeControllers = angular.module( 'homeControllers', [] );
+var homeControllers = angular.module( 'homeControllers', ['ngResource'] );
 
 
 
-homeControllers.controller('SearchController', ['$scope',
-  function ($scope) {
+homeControllers.controller('SearchController', ['$scope', '$resource',
+  function ($scope, $resource) {
+      
+      var fuzzySearch = $resource('/fs/:title');
+      
+      console.log(fuzzySearch);
       
       $scope.titleChange = function() {
-          console.log(this.movieTitle);          
+          console.log(this.movieTitle);
+          fuzzySearch.get({title:$scope.movieTitle}, function( data ) {
+              $scope.fuzzySearch = data.d;
+              angular.forEach( $scope.fuzzySearch, function(value, key) {
+                  console.log(value);
+              });
+          });
       };
+      
       
   }]);
