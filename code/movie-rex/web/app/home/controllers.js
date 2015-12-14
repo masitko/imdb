443d07e4aa2a5d498e00dab4438df8ba9a82afdc
masitko <masitko@gmail.com>
2015-12-14 17:03:32 +0000

    chnaged enter trigged search to angular

--- code/movie-rex/web/app/home/controllers.js
+++ code/movie-rex/web/app/home/controllers.js
@@ -9,17 +9,18 @@ homeControllers.controller('SearchController', ['$scope', '$resource',
         var fuzzySearch = $resource('/fs/:title');
         var enterSearch = $resource('/s/:title');
 
+        var searchContainer = [];
+
         $scope.$watch('includeTV', function () {
             console.log($scope.includeTV);
         });
-        
-        $scope.titleChange = function () {
-//            console.log(this.movieTitle);
 
-            fuzzySearch.get({title: $scope.movieTitle}, function (data) {
-                $scope.fuzzySearch = data.d;
-                angular.forEach($scope.fuzzySearch, function (title, key) {
-                    title.image = title.i === undefined ? '/img/no-poster.png' : (function () {
+        $scope.titleChange = function () {
+            var searchFor = $scope.movieTitle.replace(/ /g, '_').toLowerCase();
+            fuzzySearch.get({title: searchFor}, function (data) {
+                $scope.searchContainer = data.d;
+                angular.forEach($scope.searchContainer, function (title, key) {
+                    title.img = title.i === undefined ? '/img/no-poster.png' : (function () {
                         var ia = title.i[0].split('.');
                         var type = ia.pop();
                         ia.pop();
@@ -28,16 +29,51 @@ homeControllers.controller('SearchController', ['$scope', '$resource',
                     })();
                     title.movie = title.q && title.q.match(/(movie|feature)/);
                     title.tv = title.q && title.q.match(/TV series/);
+                    title.title = title.l;
+                    title.additional = '('+title.y+')';
                 });
             });
         };
-        
-        $scope.titleClick = function(title) {
+
+        $scope.titleEnter = function () {
+            var searchFor = $scope.movieTitle.replace(/ /g, '_').toLowerCase();
+            enterSearch.query({title: searchFor}, function (data) {
+                console.log(data);
+                $scope.searchContainer = data;
+                angular.forEach($scope.searchContainer, function (title, key) {
+                    console.log(title);  
+                    title.img = title.image === undefined ? '/img/no-poster.png' : (function () {
+                        var ia = title.image.split('.');
+                        var type = ia.pop();
+                        ia.pop();
+                        ia.push('_UX40_CR0,0,40,54_', type);
+                        return ia.join('.');
+                    })();
+                    title.movie = true;
+                    title.tv = title.q && title.q.match(/TV/);
+                });
+            });
+        };
+
+        $scope.titleClick = function (title) {
             movieRex(title.id, title.l);
         };
-            
-      
-        
+
+        $scope.titleBoxKeyUp = function (e) {
+            switch (e.which) {
+                case 13: // if Enter 
+                    if ($('li.selected').length > 0) {
+                        $('li.selected .title-container').click();
+                    }
+                    else if ($scope.movieTitle.length > 0) {
+                        $scope.titleEnter();
+//                        getSearchData(this.value.replace(/ /g, '_').toLowerCase());
+                    }
+                    break
+
+            }
+        };
+
 
 
     }]);
