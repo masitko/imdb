5d6de1a7e7aaf27c804472ea39788632fdf290ae
MAREK <masitko@gmail.com>
2015-12-14 00:03:34 +0000

    chnaged fuzzy searcg to use angular

--- code/movie-rex/web/app/home/controllers.js
+++ code/movie-rex/web/app/home/controllers.js
@@ -1,24 +1,43 @@
 
-var homeControllers = angular.module( 'homeControllers', ['ngResource'] );
+var homeControllers = angular.module('homeControllers', ['ngResource']);
 
 
 
 homeControllers.controller('SearchController', ['$scope', '$resource',
-  function ($scope, $resource) {
+    function ($scope, $resource) {
+
+        var fuzzySearch = $resource('/fs/:title');
+        var enterSearch = $resource('/s/:title');
+
+        $scope.$watch('includeTV', function () {
+            console.log($scope.includeTV);
+        });
+        
+        $scope.titleChange = function () {
+//            console.log(this.movieTitle);
+
+            fuzzySearch.get({title: $scope.movieTitle}, function (data) {
+                $scope.fuzzySearch = data.d;
+                angular.forEach($scope.fuzzySearch, function (title, key) {
+                    title.image = title.i === undefined ? '/img/no-poster.png' : (function () {
+                        var ia = title.i[0].split('.');
+                        var type = ia.pop();
+                        ia.pop();
+                        ia.push('_V1', '_UX40_CR0,0,40,54_', type);
+                        return ia.join('.');
+                    })();
+                    title.movie = title.q && title.q.match(/(movie|feature)/);
+                    title.tv = title.q && title.q.match(/TV series/);
+                });
+            });
+        };
+        
+        $scope.titleClick = function(title) {
+            movieRex(title.id, title.l);
+        };
+            
       
-      var fuzzySearch = $resource('/fs/:title');
-      
-      console.log(fuzzySearch);
-      
-      $scope.titleChange = function() {
-          console.log(this.movieTitle);
-          fuzzySearch.get({title:$scope.movieTitle}, function( data ) {
-              $scope.fuzzySearch = data.d;
-              angular.forEach( $scope.fuzzySearch, function(value, key) {
-                  console.log(value);
-              });
-          });
-      };
-      
-      
-  }]);
+        
+
+
+    }]);
